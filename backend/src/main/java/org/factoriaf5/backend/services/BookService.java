ce8edb2a63dd6244b4b15194a16f06a825e25c44
herminiocastro <hxssport@gmail.com>
2024-02-13 15:31:38 +0100

    creada estructura book java ,bookservice y bookrepository

--- backend/src/main/java/org/factoriaf5/backend/services/BookService.java
+++ backend/src/main/java/org/factoriaf5/backend/services/BookService.java
@@ -5,7 +5,6 @@ import org.factoriaf5.backend.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
