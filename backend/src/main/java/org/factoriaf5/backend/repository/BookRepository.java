bdb16096ca2514190988b3fea896a40c3f915664
herminiocastro <hxssport@gmail.com>
2024-02-13 12:51:48 +0100

    aplicationpropierties hecho

--- backend/src/main/java/org/factoriaf5/backend/repository/BookRepository.java
+++ backend/src/main/java/org/factoriaf5/backend/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package org.factoriaf5.backend.repository;
 
-public class BookRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {}
