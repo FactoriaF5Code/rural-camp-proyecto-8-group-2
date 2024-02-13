58e3c50888d02db612fee8a6191b4ea8b773b237
Marta <martalarena2002@gmail.com>
2024-02-13 12:55:51 +0100

    empezando buscador

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import "./Header.css";
 import Logo from "../../assets/logo.svg";
+import { useState } from "react";
 
 export const Header = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <>
       <main className="container">
