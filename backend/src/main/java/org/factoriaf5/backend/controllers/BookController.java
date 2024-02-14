0fac5a5e90111bcd56615ef11e2481c53e4a9179
Iciar Rodríguez <iciar@MacBook-Pro-de-Iciar.local>
2024-02-14 11:05:41 +0100

    Problemas con postgres! liadaa

--- backend/src/main/java/org/factoriaf5/backend/controllers/BookController.java
+++ backend/src/main/java/org/factoriaf5/backend/controllers/BookController.java
@@ -1,18 +1,37 @@
 package org.factoriaf5.backend.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.factoriaf5.backend.models.Book;
 import org.factoriaf5.backend.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping ("/books")
 
 public class BookController {
     @Autowired
     private BookService bookService;
 
-
-@GetMapping
+    @GetMapping
     public List<Book> findAll() {
         return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> findById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @GetMapping("/{title}")
+    public List<Book> findByTitle(@PathVariable String title) {
+        return bookService.findByTitle(title);
     }
 }
