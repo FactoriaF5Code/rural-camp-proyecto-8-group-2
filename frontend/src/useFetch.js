849cf2284819e3b2dd8b873a402b1da49a441e85
laraturrion <laturrions@gmail.com>
2024-02-15 15:21:21 +0100

    useFetch empezando

---
+++ frontend/src/useFetch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:8080/books";
+
+export function useFetch() {
+  const [data, setData] = useState(null);
+  const [needsReload, setNeedsReload] = useState(true);
+
+  useEffect(() => {
+    if(needsReload){
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) =>{
+        setData(data);
+        setNeedsReload(false);
+    });
+    }
+}, [needsReload]);
+      
+  
+  return { data };
+}
